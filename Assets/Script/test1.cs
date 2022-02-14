ccfb8747ed43c8e653469c04d744187042cbcabe
bright-clear <s20194441@gmail.com>
2022-02-14 14:48:00 +0900

    test2test

---
+++ Assets/Script/test1.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test1 : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("test1");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
